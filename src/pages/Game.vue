ffa749d046e3475d8cbeb90f80b8320b0ca434c6
GertenP <gerten.pilv@tptlive.ee>
2025-04-24 09:47:32 +0300

    parandus

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,10 +1,9 @@
 <script setup>
 import { ref } from 'vue';
-
 </script>
 
 <template>
 
-sdaasdsadas
+<img src="../assets/1000008004.jpg" alt="">
 </template>
 
--- src/pages/MainMenu.vue
+++ src/pages/MainMenu.vue
@@ -1,3 +1,4 @@
+<!-- Intro.vue -->
 <script setup>
 import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
@@ -5,19 +6,22 @@ import gunnars3 from '@/assets/gunnars3.png'
 import start from '@/assets/start.png'
 import info from '@/assets/info.png'
 import fire from '@/assets/fire.gif'
-import '@/styles/style.css' 
+import '@/styles/style.css'
 </script>
 
 <template>
-  <img class="main-logo" :src="gunnars3" />
+  <div id="intro">
+    <img class="main-logo" :src="gunnars3" />
 
-  <div class="buttons">
-    <router-link to="/game">
-      <img id="nupp1" :src="start"/>
-    </router-link>
-    <img id="nupp2" :src="info" />
+    <div class="buttons">
+      <a href="/game" target="_blank" rel="noopener noreferrer">
+        <img id="nupp1" :src="start" />
+      </a>
+      <img id="nupp2" :src="info" />
+    </div>
+
+    <div style="flex-grow: 1;"></div> <!-- Tühik surub footer alla -->
+
+    <footer class="footer"></footer>
   </div>
-
-  <footer class="footer">
-  </footer>
 </template>
