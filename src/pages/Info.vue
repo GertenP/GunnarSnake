994682c3be67400e9d8d5ff91ee433ed27ae2c25
Andri Reinek <andri.reinek@tptlive.ee>
2025-04-25 14:11:52 +0300

    Assets

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,16 +1,41 @@
 <script setup>
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+  document.body.style.backgroundColor = 'gray';
+});
+
 
 </script>
 
 <template>
-    
-    
-    <img src="../assets/Andri With Frame 1.png" alt="">
-    <img src="../assets/Gerten With Frame 1.png" alt="">
-    <img src="../assets/Mark With Frame 1.png" alt="">
-    <img src="../assets/Gunnar To Info.png" alt="">
+    <div class="button">
+      <router-link to="/">
+        <button>< Avalehele</button>
+      </router-link>
+    </div>
+    <header>
+      <p>INFO</p>
+      <img src="../assets/GameOfTheYear.png" alt="">
+    </header>
+
 
 
 
 </template>
+
+<style scoped>
+button{
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    color: white;
+}
+</style>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap');
+</style>
 
